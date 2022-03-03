e2e78b8422b41ac684d2851585f169b88c5f589a
Sinu Sekhar <sinusekhar@gmail.com>
2022-03-03 01:29:17 +0000

    Modified the app

--- app.py
+++ app.py
@@ -7,12 +7,12 @@ import plotly.graph_objs as go
 
 ########### Define your variables ######
 
-myheading1='How to use callbacks'
+myheading1='Callback demo from Sinu!'
 tabtitle = 'xkcd'
-list_of_options=['box plot', 'correlation', 'git commit', 'scatterplot']
-list_of_images=['outlier.png', 'correlation.png', 'gitcommit.jpg', 'scatterplot.png', 'good_code.png']
+list_of_options=['box plot', 'correlation', 'git commit', 'scatterplot','math_symbols']
+list_of_images=['outlier.png', 'correlation.png', 'gitcommit.jpg', 'scatterplot.png', 'good_code.png','mathematical_symbol_fight.png']
 sourceurl = 'https://xkcd.com/'
-githublink = 'https://github.com/plotly-dash-apps/203-radio-callbacks'
+githublink = 'https://github.com/sinusekhar/203-radio-callbacks'
 
 
 ########## Set up the chart
